970b9b0c5ed1ac64f2d8852f25d4be9200532afe
niartenyaw <amwayne90@gmail.com>
2018-10-15 13:17:49 -0700

    Add more logs

--- plugins/metalsmith-webpack-custom.js
+++ plugins/metalsmith-webpack-custom.js
@@ -3,31 +3,27 @@ var webpack = require('webpack');
 
 function plugin(options) {
   return function metalsmithWebpackPlugin(files, metalsmith, done) {
-    console.log("In webpack middleware");
     let webpackOptions;
     if (typeof options === 'string') {
       const configPath = path.resolve(path.dirname(require.main.filename), options);
-      console.log("Pre options load");
       webpackOptions = require(configPath);
       console.log("Post options load");
     } else {
       webpackOptions = options;
     }
     if (webpackOptions) {
+      console.log("webpack options found, starting webpack");
+      console.log(webpack);
+      console.log(webpackOptions);
       webpack(webpackOptions, (err, stats) => {
         const info = stats.toString({ chunkModules: false, colors: true });
         if (err || stats.hasErrors()) {
+          console.log("webpack error");
           console.error(err);
         }
-        console.log("info");
         console.log(info);
 
-        console.log("files");
-        console.log(files);
-
         for (var file in files) {
-          console.log("file");
-          console.log(file);
           files[file].webpackStats = stats;
         }
 
